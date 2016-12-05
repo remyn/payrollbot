b69ec6b4f6511a6a20dbbde5250945a7d92adc77
mornen <mniem22@gmail.com>
2016-12-05 15:11:25 +1300

    Added Speech API

--- ReckonTwo/ReckonTwo/Controllers/HomeController.cs
+++ ReckonTwo/ReckonTwo/Controllers/HomeController.cs
@@ -1,10 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using ReckonTwo.Helpers;
+using System.Web.Mvc;
 
 namespace ReckonTwo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> TextToSpeech()
+        {
+            var api = new TextToSpeechApiHelper("bc9f4dfe324e474bb37b8f2480f1b7a4");
+            await api.StartTextToSpeechAPI(api);
+
+            return View();
+        }
+
+        public ActionResult SpeechToText()
         {
             return View();
         }
