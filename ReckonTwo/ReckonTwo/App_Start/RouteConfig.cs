fa0c2d24cb4c523684842d570630ea8ddddd0de4
mornen <mornen@NZ-MORNEN-WKS>
2016-12-09 12:24:46 +1300

    reset chatbox on signout

--- ReckonTwo/ReckonTwo/App_Start/RouteConfig.cs
+++ ReckonTwo/ReckonTwo/App_Start/RouteConfig.cs
@@ -30,7 +30,7 @@ namespace ReckonTwo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "web", action = "login", id = UrlParameter.Optional }
+                defaults: new { controller = "home", action = "login", id = UrlParameter.Optional }
             );
         }
     }
--- ReckonTwo/ReckonTwo/Controllers/HomeController.cs
+++ ReckonTwo/ReckonTwo/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using ReckonTwo.Helpers;
 using ReckonTwo.Models;
+using ReckonTwo.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using System.Web.UI;
 
 namespace ReckonTwo.Controllers
@@ -19,17 +22,14 @@ namespace ReckonTwo.Controllers
         private const string _botSecretKey = "zkzhHeiTCjc.cwA.36s.6rsMSclswl2chHVZUIUbztcAwzvpFhx3Lk7reN5kJW0";
         private const string _azureStorageEndpoint = "DefaultEndpointsProtocol=https;AccountName=mornestorageaccount;AccountKey=GIQOmCrdQslI9rAir4/Kajzr8UwZNkRGCn0TfG0rIY4GyvVgU3Ejci/88HgEIXMIUkVwB3bfUxvfbAH7DNfQ7w==";
         private const string _speechApiKey = "42d4e9b82b4e43108387e5458216ab00";
-        private static readonly IList<CommentModel> _comments;
+        private static IList<CommentModel> _comments;
         private static SpeechApiHelper _speechApi;
-
-        static HomeController()
-        {
-            _comments = new List<CommentModel>();
-            _speechApi = new SpeechApiHelper(_speechApiKey);
-        }
 
         public ActionResult Index()
         {
+            _comments = new List<CommentModel>();
+            _speechApi = new SpeechApiHelper(_speechApiKey);
+
             var userGuid = GetLoggedInUserID();
             var userFullName = GetLoggedInUserFullName();
 
@@ -187,6 +187,75 @@ namespace ReckonTwo.Controllers
 
             chat.Watermark = watermark;
             return chat;
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new LogInModel());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "Email,Password")] LogInModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var encPass = Helper.HashPassword(user.Password.Trim());
+
+                if (!Helper.ConfirmLoginDetails(encPass, user.Email.Trim()))
+                {
+                    ViewBag.ErrorMessage = "Email or Password is incorrect. Please try again.";
+                    return View(user);
+                }
+
+                User loggedInUser = new User
+                {
+                    UserID = Guid.NewGuid(),
+                    Email = user.Email.Trim(),
+                    FirstName = "Joe",
+                    LastName = "Bloggs",
+                    FlagAdmin = true,
+                    FlagDeleted = false,
+                    Password = encPass
+                };
+
+                DateTime cookieIssuedDate = DateTime.Now;
+
+                var ticket = new FormsAuthenticationTicket(0,
+                                 loggedInUser.Email,
+                                 cookieIssuedDate,
+                                 cookieIssuedDate.AddMinutes(FormsAuthentication.Timeout.TotalMinutes),
+                                 false,
+                                 string.Concat(loggedInUser.FullName, "|", loggedInUser.UserID.ToString()),
+                                 FormsAuthentication.FormsCookiePath);
+
+                string encryptedCookieContent = FormsAuthentication.Encrypt(ticket);
+
+                var formsAuthenticationTicketCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedCookieContent)
+                {
+                    Domain = FormsAuthentication.CookieDomain,
+                    Path = FormsAuthentication.FormsCookiePath,
+                    HttpOnly = true,
+                    Secure = false
+                };
+
+                System.Web.HttpContext.Current.Response.Cookies.Add(formsAuthenticationTicketCookie);
+
+                return RedirectToAction("index", "home", null);
+            }
+
+            return View(new LogInModel());
+        }
+
+        public ActionResult LogOff()
+        {
+            _comments = null;
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("login", "home");
         }
     }
 }
